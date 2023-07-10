9171cb7909ef7b10a2286f40f9c1b54ce96b2e66
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2023-07-10 18:02:53 +0200

    Added file opened event handler

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,7 +8,10 @@
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 using System.Diagnostics;
+using Microsoft.Windows.AppLifecycle;
+using System.Linq;
 using Windows.ApplicationModel.Activation;
+using Windows.Storage;
 
 namespace Pyrux
 {
@@ -32,6 +35,23 @@ namespace Pyrux
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            AppActivationArguments appActivationArguments = AppInstance.GetCurrent().GetActivatedEventArgs();
+
+            if (appActivationArguments.Kind is ExtendedActivationKind.File &&
+                appActivationArguments.Data is IFileActivatedEventArgs fileActivatedEventArgs &&
+                fileActivatedEventArgs.Files.FirstOrDefault() is IStorageFile storageFile
+                )
+            {
+                using (StreamReader sr = new(storageFile.Path))
+                {
+                    string file = sr.ReadToEnd();
+                    using (StreamWriter sw = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "FileContent.txt")))
+                    {
+                        sw.Write(file);
+                    }
+                }
+            }
+
             m_window = new MainWindow();
             m_window.Activate();
         }
