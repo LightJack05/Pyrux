0349d97a2faba24fd48d015d7533b839675cc35d
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2023-07-15 16:55:45 +0200

    Implemented settings navigation

--- Pages/SettingsPage.xaml.cs
+++ Pages/SettingsPage.xaml.cs
@@ -5,6 +5,8 @@
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+using Pyrux.Pages.SettingsPages;
+
 namespace Pyrux.Pages
 {
     /// <summary>
@@ -12,6 +14,11 @@ namespace Pyrux.Pages
     /// </summary>
     public sealed partial class SettingsPage : Page
     {
+        public List<(string Tag, Type Page)> contentDictionary = new()
+        {
+            ("movement",typeof(ExecutionSettings))
+        };
+
         public SettingsPage()
         {
             this.InitializeComponent();
@@ -19,12 +26,37 @@ namespace Pyrux.Pages
 
         private void ngvSettings_Loaded(object sender, RoutedEventArgs e)
         {
-
+            ngvSettings.SelectedItem = ngvSettings.MenuItems[0];
+            NavViewNavigate("movement", new Microsoft.UI.Xaml.Media.Animation.EntranceNavigationTransitionInfo());
         }
 
         private void ngvSettings_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
+            if (args.InvokedItemContainer != null)
+            {
+                string navItemTag = args.InvokedItemContainer.Tag.ToString();
+                NavViewNavigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
+        }
 
+        /// <summary>
+        /// Navigate the content frame to the selected page.
+        /// </summary>
+        /// <param name="navItemTag">Tag of the selected page.</param>
+        /// <param name="transitionInfo">Transitioninfo for transition animation.</param>
+        public void NavViewNavigate(string navItemTag, Microsoft.UI.Xaml.Media.Animation.NavigationTransitionInfo transitionInfo)
+        {
+            Type page = null;
+
+            (string Tag, Type Page) item = contentDictionary.FirstOrDefault(p => p.Tag.Equals(navItemTag));
+            page = item.Page;
+
+
+            Type preNavPageType = ctfSettings.CurrentSourcePageType;
+            if (!(page is null) && !Type.Equals(preNavPageType, page))
+            {
+                ctfSettings.Navigate(page, null, transitionInfo);
+            }
         }
     }
 }
