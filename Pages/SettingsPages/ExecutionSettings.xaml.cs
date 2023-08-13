82fde53f570e8a41cfe97c3e26649de8b9a6661d
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2023-08-13 15:59:52 +0200

    Changed slider in settings to combo box

--- Pages/SettingsPages/ExecutionSettings.xaml.cs
+++ Pages/SettingsPages/ExecutionSettings.xaml.cs
@@ -24,12 +24,27 @@ namespace Pyrux.Pages.SettingsPages
     /// </summary>
     public sealed partial class ExecutionSettings : Page
     {
+        public Dictionary<string, int> DelayStringDictionary { get; set; } = new Dictionary<string, int>()
+        {
+            {"0.1s", 100 },
+            {"0.2s", 200 },
+            {"0.5s", 500 },
+            {"1s", 1000 },
+            {"2s", 2000 },
+            {"3s", 3000 },
+            {"5s", 5000 },
+            {"7s", 7000 },
+            {"10s", 10000 }
+        };
+
         public ExecutionSettings()
         {
             int delayBeforeAutoReset = PyruxSettings.DelayBeforeAutoReset;
             this.InitializeComponent();
             PyruxSettings.DelayBeforeAutoReset = delayBeforeAutoReset;
-            sldDelayBeforeReset.Value = delayBeforeAutoReset;
+            //sldDelayBeforeReset.Value = delayBeforeAutoReset;
+            string ComboBoxItemString = DelayStringDictionary.FirstOrDefault((item) => item.Value == delayBeforeAutoReset).Key;
+            cbxResetDelay.SelectedItem = cbxResetDelay.Items.FirstOrDefault((item) => (string)((ComboBoxItem)item).Content == ComboBoxItemString);
             UpdateSettingsDisplay();
         }
 
@@ -42,27 +57,33 @@ namespace Pyrux.Pages.SettingsPages
         {
             PyruxSettings.AutoRestartOnFinish = tswAutoReset.IsOn;
             UpdateSettingsDisplay();
-            
+
         }
         private void UpdateSettingsDisplay()
         {
             tswAutoReset.IsOn = PyruxSettings.AutoRestartOnFinish;
             tswAddDelay.IsOn = PyruxSettings.AddDelayBeforeAutoReset;
             tswAddDelay.IsEnabled = PyruxSettings.AutoRestartOnFinish;
-            sldDelayBeforeReset.IsEnabled = PyruxSettings.AutoRestartOnFinish && PyruxSettings.AddDelayBeforeAutoReset;
+            cbxResetDelay.IsEnabled = PyruxSettings.AutoRestartOnFinish && PyruxSettings.AddDelayBeforeAutoReset;
+            //sldDelayBeforeReset.IsEnabled = PyruxSettings.AutoRestartOnFinish && PyruxSettings.AddDelayBeforeAutoReset;
             PyruxSettings.SaveSettings();
         }
 
-        private void sldDelayBeforeReset_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
-        {
-            PyruxSettings.DelayBeforeAutoReset = (int)sldDelayBeforeReset.Value;
-            UpdateSettingsDisplay();
-        }
+        //private void sldDelayBeforeReset_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        //{
+        //    //PyruxSettings.DelayBeforeAutoReset = (int)sldDelayBeforeReset.Value;
+        //    UpdateSettingsDisplay();
+        //}
 
         private void tswAddDelay_Toggled(object sender, RoutedEventArgs e)
         {
             PyruxSettings.AddDelayBeforeAutoReset = tswAddDelay.IsOn;
             UpdateSettingsDisplay();
+        }
+
+        private void cbxResetDelay_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PyruxSettings.DelayBeforeAutoReset = DelayStringDictionary[(string)((ComboBoxItem)(cbxResetDelay.SelectedItem)).Content];
         }
     }
 }
